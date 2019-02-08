d8755dc2d3df10b876536aaa89c852d61ca4b932
Colin Campbell <34920184+ColinACampbell@users.noreply.github.com>
2019-02-08 09:14:59 -0500

    TeamMylesTest.java
    Code written for the tray lift motor and servos for tray dispenser.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMylesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMylesTest.java
@@ -4,15 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
-public class TeamMylesTest extends LinearOpMode {
-    private DcMotor FrontR,FrontL,BackR,BackL, IntakeL,IntakeR;
+public class TeamMylesTest extends LinearOpMode
+{
+
+    private DcMotor FrontR,FrontL,BackR,BackL, IntakeL,IntakeR, trayLift;
+    private Servo trayDispenserL, trayDispenserR;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+        // Drive train and intake system
         FrontL = hardwareMap.dcMotor.get("Fleft");
         FrontR = hardwareMap.dcMotor.get("Fright");
         BackR = hardwareMap.dcMotor.get("BackRight");
@@ -26,11 +31,19 @@ public class TeamMylesTest extends LinearOpMode {
         IntakeL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         IntakeR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackL.setDirection(DcMotor.Direction.REVERSE);
+
+        // Tray control
+        trayDispenserL = hardwareMap.servo.get("TrayDispLeft");
+        trayDispenserR = hardwareMap.servo.get("TrayDispRight");
+        trayDispenserL.setDirection(Servo.Direction.REVERSE); // Both servos are facing each other
+        trayLift = hardwareMap.dcMotor.get("TrayLift");
+
         waitForStart();
 
         while (opModeIsActive()){
             MovementSys();
             CollectionSys();
+            trayControlSys();
             telemetry.update();
         }
     }
@@ -58,6 +71,30 @@ public class TeamMylesTest extends LinearOpMode {
         {
             IntakeL.setPower(0);
             IntakeR.setPower(0);
+        }
+    }
+
+    private void trayControlSys()
+    {
+        // control the tray dispenser
+        if (gamepad1.dpad_up)
+        {
+            trayDispenserL.setPosition(1.0);
+            trayDispenserR.setPosition(1.0);
+        } else if (gamepad1.dpad_down)
+        {
+            trayDispenserL.setPosition(0);
+            trayDispenserR.setPosition(0);
+        }
+
+
+        // control the tray lift
+        if (gamepad1.dpad_right)
+        {
+            trayLift.setPower(1.0);
+        } else if (gamepad1.dpad_left)
+        {
+            trayLift.setPower(-1.0);
         }
     }
 }
