d06c86369265de2ace2b2faa3d51c878d7a21f2a
Mick Meal <45320686+MickMeal@users.noreply.github.com>
2019-02-14 11:55:39 -0500

    Don't Override Unless necessary

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavonTest.java
@@ -8,31 +8,36 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 
-// @author Colin 'Bart' Campbell & Javon 'Utility Pole' Peart
+// @Authors Chavaughn Wilkins, Colin  Campbell & Javon Peart
 @TeleOp(name = "JavonTeamMyles")
+
 public class JavonTest extends LinearOpMode
 {
 
-    private DcMotor FrontLeft, BackLeft, FrontRight, BackRight, ChainLift;
-    private Servo trayDispL, trayDispR,IntakeR,IntakeL; // Servos to control tray dispenser
+    private DcMotor FrontLeft, BackLeft, FrontRight, BackRight, ChainLift, HookLift;
+    private Servo trayDispL, trayDispR,IntakeR,IntakeL;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+        //Wheel Motors
         FrontLeft =  hardwareMap.dcMotor.get("FrontLeft");
         FrontRight =  hardwareMap.dcMotor.get("FrontRight");
         BackLeft = hardwareMap.dcMotor.get("BackLeft");
         BackRight = hardwareMap.dcMotor.get("BackRight");
 
+        //Collection Devices
+        HookLift = hardwareMap.dcMotor.get("HookLift");
         ChainLift = hardwareMap.dcMotor.get("ChainLift");
         trayDispL = hardwareMap.servo.get("TrayDispL");
         trayDispR = hardwareMap.servo.get("TrayDispR");
         trayDispL.setDirection(Servo.Direction.REVERSE);
         // TODO inverse either of the servos if anything goes wrong in testing
 
+        //Lowest Intake Servos
         IntakeL = hardwareMap.servo.get("IntakeL");
         IntakeR = hardwareMap.servo.get("IntakeR");
-        //TODO : inverse either of the servos incase if anything goes wrong in testing
+        //TODO : inverse either of the servos in case if anything goes wrong in testing
 
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -46,6 +51,7 @@ public class JavonTest extends LinearOpMode
             MovementSys();
             CollectionSys();
             trayControlSys();
+            HookSys();
             telemetry.update();
         }
 
@@ -55,62 +61,80 @@ public class JavonTest extends LinearOpMode
         telemetry.addLine("************Status: Running***********");
 
         double Power1, Power2;
-        Power1 = Range.clip(gamepad1.left_stick_y, -1, 1);
-        Power2 = Range.clip(gamepad1.right_stick_y, -1, 1);
+        Power1 = Range.clip(gamepad1.left_stick_y, -0.8, 0.8);
+        Power2 = Range.clip(gamepad1.right_stick_y, -0.8, 0.8);
 
+        //Powers Set for Wheel Motors
         FrontLeft.setPower(Power2);
         BackLeft.setPower (Power2);
-
         BackRight.setPower (-Power1);
         FrontRight.setPower(-Power1);
 
+
+
         telemetry.addLine("Robots as seen...");
-        telemetry.addData("In their Natural Habitat", Power1);
+        telemetry.addData("In their Natural Habitat ", Power1);
     }
 
     private void CollectionSys(){
-        if (gamepad1.a){
+        if (gamepad1.right_bumper){
             IntakeL.setPosition(-1);
-            IntakeR.setPosition(-1);
-        }
-        else if (gamepad1.b)
-        {
-            IntakeL.setPosition(1);
             IntakeR.setPosition(1);
         }
-        else
-        {
+
+        else if (gamepad1.left_bumper){
+            IntakeL.setPosition(1);
+            IntakeR.setPosition(-1);
+        }
+
+        else{
             IntakeL.setPosition(0.5);
             IntakeR.setPosition(0.5);
         }
     }
 
-    private void trayControlSys()
-    {
-
-        // Events for chain lift
+    private void trayControlSys(){
+        // Control and Power for chain lift
         if (gamepad1.dpad_up){
             ChainLift.setPower(1);
         }
+
         else if(gamepad1.dpad_down){
             ChainLift.setPower(-1);
         }
+
         else{
             ChainLift.setPower(0);
         }
 
-        //Events for tray dispenser servos
+        //Control and Power for Tray Dispenser servos
         if (gamepad1.dpad_left) {
             trayDispL.setPosition(1.0);
             trayDispR.setPosition(1.0);
         }
+
         else if (gamepad1.dpad_right) {
             trayDispL.setPosition(-1.0);
             trayDispR.setPosition(-1.0);
-        } else
-        {
+        }
+
+        else{
             trayDispL.setPosition(0.5);
             trayDispR.setPosition(0.5);
+        }
+    }
+
+    private void HookSys(){
+        //Control for Hook Lift
+        if (gamepad1.y) {
+            HookLift.setPower(1);
+        }
+
+        else if ( gamepad1.x) {
+            HookLift.setPower(-1);
+        }
+        else {
+            HookLift.setPower(0);
         }
     }
 }
