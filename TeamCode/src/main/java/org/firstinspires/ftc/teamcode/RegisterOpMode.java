df1176d3f4bb2dbae7bd2f765986b6f2748893ab
Chavaughn <chavaughwilks@yahoo.com>
2019-02-25 15:55:08 -0500

    Update RegisterOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisterOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisterOpMode.java
@@ -3,13 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
 
-import org.firstinspires.ftc.teamcode.QuickAutoTests.GoldAlignExample;
-import org.firstinspires.ftc.teamcode.QuickAutoTests.SamplingOrderExample;
-
 public class RegisterOpMode {
     @OpModeRegistrar
     public static void registerMyOpModes(OpModeManager manager){
-
 
     }
 }
