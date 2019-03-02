fe94d78d3469a979f378d315a658b3961052df3d
David-LA-Smith <David-LA-Smith@users.noreply.github.com>
2019-03-02 18:03:28 -0500

    Chav Edit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp6899/Autonomous6899.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp6899/Autonomous6899.java
@@ -23,6 +23,7 @@ public class Autonomous6899 extends LinearOpMode {
 
     private double getruntime;
 
+    private int y = 2;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -30,8 +31,8 @@ public class Autonomous6899 extends LinearOpMode {
         detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
         detector.useDefaults(); // Set detector to use default settings
         //tuning
-        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
-        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.alignSize = 250; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = -2; // How far from center frame to offset this alignment zone.
         detector.downscale = 0.4; // How much to downscale the input frames
 
         detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
@@ -42,59 +43,87 @@ public class Autonomous6899 extends LinearOpMode {
         detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
         Bot.init(hardwareMap);
 
+        int x = 2;
+
         detector.enable();
         waitForStart();
 
 
         while (opModeIsActive()) {
 
-            runtime.reset();
-            if (detector.getAligned()){
-                telemetry.addLine("Found Cube!");
-                telemetry.update();
-            }
-            if (!detector.getAligned() && runtime.seconds() <= 3 && opModeIsActive()) {
+            if (!detector.getAligned() || !detector.isFound() && opModeIsActive()) {
                 telemetry.addLine("Looking for Cube first side");
                 telemetry.update();
                 //Looking for Cube
-                Rotate(1, .2, 2000);
+                runtime.reset();
+                RotateAli(2, .1, 1800);
                 if (detector.getAligned()) {
                     telemetry.addLine("Found Cube! 1st side");
                     telemetry.update();
                     getruntime = runtime.seconds();
+                    x =1;
+                    y=4;
                 }
 
                 //Opposite direction
                 telemetry.addLine("Looking for Cube second side");
                 telemetry.update();
-                Rotate(1, -.2, 2000);
+                runtime.reset();
+                RotateAli(2, -.1, 2500);
                 if (detector.getAligned()) {
+                    x = 1;
+                    y=3;
                     telemetry.addLine("Found Cube! 2nd side");
                     telemetry.update();
                     getruntime = runtime.seconds();
                 }
+                if (detector.getAligned() && x==1) {
+                    telemetry.addLine("Found Cube!");
+                    telemetry.update();
+
+                    x=2;
+                    telemetry.addLine("Grabbing the Cube!");
+                    telemetry.update();
+                    //Goes to cube and grabs it
+                    Bot.IntakeL.setPosition(0);
+                    Bot.IntakeR.setPosition(1);
+                    RunToPosDrive(1, 0.25, 3000);
+
+                    telemetry.addLine("Moving back to Original Pos.");
+                    telemetry.update();
+                    //Moves back to original position
+                    Bot.IntakeL.setPosition(0.5);
+                    Bot.IntakeR.setPosition(0.5);
+                    RunToPosDrive(1, -0.25, 3000);
+                    if (y == 3) {
+                        runtime.reset();
+                        RotateBack(1, -0.25, 1000);
+                    }else{
+                        runtime.reset();
+                        RotateBack(1, 0.25, 1000);
+                    }
+                }
+            }
+            if (detector.getAligned() && x != 2) {
+                telemetry.addLine("Found Cube!");
+                telemetry.update();
+
+                telemetry.addLine("Grabbing the Cube!");
+                telemetry.update();
+                //Goes to cube and grabs it
+                Bot.IntakeL.setPosition(0);
+                Bot.IntakeR.setPosition(1);
+                RunToPosDrive(1, 0.25, 3000);
+
+                telemetry.addLine("Moving back to Original Pos.");
+                telemetry.update();
+                //Moves back to original position
+                Bot.IntakeL.setPosition(0.5);
+                Bot.IntakeR.setPosition(0.5);
+                RunToPosDrive(1, -0.25, 3000);
             }
 
-            telemetry.addLine("Grabbing the Cube!");
-            telemetry.update();
-            //Goes to cube and grabs it
-            RunToPosDrive(4, 0.5, 3000);
-            Bot.IntakeL.setPosition(1);
-            Bot.IntakeR.setPosition(0);
-
-            telemetry.addLine("Moving back to Original Pos.");
-            telemetry.update();
-            //Moves back to original position
-            RunToPosDrive(4, -0.5, 3000);
-            Bot.IntakeL.setPosition(0.5);
-            Bot.IntakeR.setPosition(0.5);
-
-            //Rotates back into original position
-            runtime.reset();
-
-                Rotate(1, 0.2, 1000);
-
-            //Just for testing - stops Opmode
+            RotateAli(2, .1, 2000);
         }
     }
 
@@ -148,5 +177,60 @@ public class Autonomous6899 extends LinearOpMode {
         Bot.BackRight.setPower(0);
         Bot.BackLeft.setPower(0);
     }
+    private void RotateAli(int inches,double power, int sleeptimer){
+       while (!detector.getAligned() && opModeIsActive() && runtime.seconds() < 1.2 && y ==2) {
+           Bot.FrontLeft.setTargetPosition(inches);
+           Bot.FrontRight.setTargetPosition(inches);
+           Bot.BackRight.setTargetPosition(inches);
+           Bot.BackLeft.setTargetPosition(inches);
+           Bot.FrontRight.setPower(-power);
+           Bot.BackLeft.setPower(power);
+           Bot.FrontLeft.setPower(power);
+           Bot.BackRight.setPower(-power);
+           if (detector.getAligned()) {
+               telemetry.addLine("Found Cube!");
+               telemetry.update();
+               getruntime = runtime.seconds();
+               ZeroDrive();
+           }
+           if (detector.getAligned()){
+               y=3;
+           }
+       }
+        while (!detector.getAligned() && opModeIsActive() && runtime.seconds() < 3.2 && y ==3) {
+            Bot.FrontLeft.setTargetPosition(inches);
+            Bot.FrontRight.setTargetPosition(inches);
+            Bot.BackRight.setTargetPosition(inches);
+            Bot.BackLeft.setTargetPosition(inches);
+            Bot.FrontRight.setPower(-power);
+            Bot.BackLeft.setPower(power);
+            Bot.FrontLeft.setPower(power);
+            Bot.BackRight.setPower(-power);
+            if (detector.getAligned()) {
+                telemetry.addLine("Found Cube!");
+                telemetry.update();
+                getruntime = runtime.seconds();
+                ZeroDrive();
+            }
+        }
+        sleep(sleeptimer);
+        ZeroDrive();
+        RunwithEncAll();
+    }
+
+    private void RotateBack(int inches,double power,int sleeptimer){
+                Bot.FrontLeft.setTargetPosition(inches);
+                Bot.FrontRight.setTargetPosition(inches);
+                Bot.BackRight.setTargetPosition(inches);
+                Bot.BackLeft.setTargetPosition(inches);
+                Bot.FrontRight.setPower(-power);
+                Bot.BackLeft.setPower(power);
+                Bot.FrontLeft.setPower(power);
+                Bot.BackRight.setPower(-power);
+        sleep(sleeptimer);
+        ZeroDrive();
+        RunwithEncAll();
+    }
+
 }
 
