a4d91ee6c1836e62df6fad963f27339a77a87847
David-LA-Smith <David-LA-Smith@users.noreply.github.com>
2019-03-02 12:31:47 -0500

    Update Mecanum.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -54,8 +54,8 @@ public class Mecanum {
                                           double leftStickY,
                                           double rightStickX,
                                           double rightStickY) {
-        double vD = Math.min(Math.sqrt(Math.pow(leftStickY, 2) +
-                                       Math.pow(leftStickX, 2)),
+        double vD = Math.min(Math.sqrt(Math.pow(leftStickX, 2) +
+                                       Math.pow(leftStickY, 2)),
                              1);
         double thetaD = Math.atan2(leftStickX, leftStickY);
         double vTheta = -rightStickX;
