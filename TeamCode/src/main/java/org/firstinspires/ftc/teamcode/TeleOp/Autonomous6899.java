e1c7756f2e0441b2da08815e895423c72eee8b66
Chavaughn <chavaughwilks@yahoo.com>
2019-03-03 18:14:09 -0500

    name change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Autonomous6899.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Autonomous6899.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp6899;
+package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
