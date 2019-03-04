b772262935ce870347d0672296a4227f90076386
Chavaughn <chavaughwilks@yahoo.com>
2019-03-03 22:27:36 -0500

    Autonomous. Finished up to knocking off correct jewel.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Autonomous6899.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Autonomous6899.java
@@ -8,10 +8,28 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
+
 @Autonomous(name = "Autonomous 6899")
 public class Autonomous6899 extends LinearOpMode {
 
-    Hardware6899 Bot = new Hardware6899();
+    private Hardware6899 Bot = new Hardware6899();
 
     private enum CompetitionLegs{
         Delatch,Vuforia, Mineral, Marker, Crater, Finish}
@@ -25,8 +43,26 @@ public class Autonomous6899 extends LinearOpMode {
 
     private int y = 2;
 
+    private String Rucks;
+
+    private static final String VUFORIA_KEY = "AU3/vpT/////AAABmSeNX5cWlUasitZlrRdsr6UXUxmuHVz0w0+A2hsEHEDmOXhnedTh/pyJSdeSzznNkYLUtxa5SEUqrwClVdYYAOZTo8zEUGDNUdfUSd8mKmjezFt64DRUyPb2EwE+LG172I4yK/jdryp1sHIfD1uVt3pqaz438j/dRQTnqED2yWQ28inuRMv0je2GcLDYWeeIrcKKwaMkSHYGikcu6knLTQSXcaqcdJkXa+d2teUqzGcH5pmg0PzzTffyjIRuj342Ivwm4+e8hAY1IwhsyTHHhvs+7w6G1qRX+yR4udJh4S6lUQU/ZYwn/7IQJqATkM7uOeBTBm4UNX1uz+8uRP0ZS2qJFWb+zagMlA34x+/9+hVh";
+
+    private static final float mmPerInch        = 25.4f;
+    private static final float mmFTCFieldWidth  = (12*6) * mmPerInch;       // the width of the FTC field (from the center point to the outer panels)
+    private static final float mmTargetHeight   = (6) * mmPerInch;          // the height of the center of the target image above the floor
+
+    // Select which camera you want use.  The FRONT camera is the one on the same side as the screen.
+    // Valid choices are:  BACK or FRONT
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+
+    private OpenGLMatrix lastLocation = null;
+    private boolean targetVisible = false;
+
+    private VuforiaLocalizer vuforia;
+
     @Override
     public void runOpMode() throws InterruptedException {
+
         detector = new GoldAlignDetector(); // Create detector
         detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
         detector.useDefaults(); // Set detector to use default settings
@@ -38,16 +74,85 @@ public class Autonomous6899 extends LinearOpMode {
         detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
         //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
         detector.maxAreaScorer.weight = 0.005; //
-
         detector.ratioScorer.weight = 5; //
         detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+        //Vuforia
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId",
+                "id", hardwareMap.appContext.getPackageName());
+//        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        //TODO Use parameterless if you do not need to see the camera commented out below
+         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY ;
+        parameters.cameraDirection   = CAMERA_CHOICE;
+
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        //TODO Use the data here to get the different locations on the field.
+        VuforiaTrackables targetsRoverRuckus = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
+        VuforiaTrackable blueRover = targetsRoverRuckus.get(0);
+        blueRover.setName("Blue-Rover");
+        VuforiaTrackable redFootprint = targetsRoverRuckus.get(1);
+        redFootprint.setName("Red-Footprint");
+        VuforiaTrackable frontCraters = targetsRoverRuckus.get(2);
+        frontCraters.setName("Front-Craters");
+        VuforiaTrackable backSpace = targetsRoverRuckus.get(3);
+        backSpace.setName("Back-Space");
+
+        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll(targetsRoverRuckus);
+
+        //Translation
+        //Blue Rover
+        OpenGLMatrix blueRoverLocationOnField = OpenGLMatrix
+                .translation(0, mmFTCFieldWidth, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0));
+        blueRover.setLocation(blueRoverLocationOnField);
+
+        //Red Footprint
+        OpenGLMatrix redFootprintLocationOnField = OpenGLMatrix
+                .translation(0, -mmFTCFieldWidth, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180));
+        redFootprint.setLocation(redFootprintLocationOnField);
+
+        //Front Craters
+        OpenGLMatrix frontCratersLocationOnField = OpenGLMatrix
+                .translation(-mmFTCFieldWidth, 0, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90));
+        frontCraters.setLocation(frontCratersLocationOnField);
+
+        //Back Space
+        OpenGLMatrix backSpaceLocationOnField = OpenGLMatrix
+                .translation(mmFTCFieldWidth, 0, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90));
+        backSpace.setLocation(backSpaceLocationOnField);
+
+        final int CAMERA_FORWARD_DISPLACEMENT  = 110;   // eg: Camera is 110 mm in front of robot center
+        final int CAMERA_VERTICAL_DISPLACEMENT = 200;   // eg: Camera is 200 mm above ground
+        final int CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
+
+        OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES,
+                        CAMERA_CHOICE == FRONT ? 90 : -90, 0, 0));
+
+        //Let all the trackable listeners know where the phone is
+        for (VuforiaTrackable trackable : allTrackables)
+        {
+            ((VuforiaTrackableDefaultListener)trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        }
+
         Bot.init(hardwareMap);
+
 
         int x = 2;
 
         detector.enable();
+        targetVisible = false;
+        targetsRoverRuckus.activate();
         waitForStart();
-
 
         while (opModeIsActive()) {
 
@@ -124,6 +229,41 @@ public class Autonomous6899 extends LinearOpMode {
             }
 
             RotateAli(2, .1, 2000);
+
+            //Vuforia time!
+            for (VuforiaTrackable trackable : allTrackables) {
+                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                    telemetry.addData("Visible Target", trackable.getName());
+                    Rucks = trackable.getName();
+
+                    // getUpdatedRobotLocation() will return null if no new information is available since
+                    // the last time that call was made, or if the trackable is not currently visible.
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                    if (robotLocationTransform != null) {
+                        lastLocation = robotLocationTransform;
+                    }
+                    break;
+                }
+                //Original Pos
+                RotateAli(2, -.1, 2000);
+                switch (Rucks){
+                    case "Blue-Rover":
+                        BlueRover();
+                        break;
+                    case "Red-Footprint":
+                        RedFootprint();
+                        break;
+                    case "Front-Craters":
+                        FrontCraters();
+                        break;
+                    case "Back-Space":
+                        BackSpace();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 
@@ -177,6 +317,7 @@ public class Autonomous6899 extends LinearOpMode {
         Bot.BackRight.setPower(0);
         Bot.BackLeft.setPower(0);
     }
+
     private void RotateAli(int inches,double power, int sleeptimer){
        while (!detector.getAligned() && opModeIsActive() && runtime.seconds() < 1.2 && y ==2) {
            Bot.FrontLeft.setTargetPosition(inches);
@@ -230,6 +371,142 @@ public class Autonomous6899 extends LinearOpMode {
         sleep(sleeptimer);
         ZeroDrive();
         RunwithEncAll();
+    }
+
+    private void DepositMarker(){
+        /*TODO Add to config file and hardware code:
+TODO                     The Servo
+         */
+        sleep(1000);
+    }
+
+    private void Step4(){
+        RunToPosDrive(3,-0.5,2000);
+        DepositMarker();
+    }
+
+    private void Step5(){
+        RunToPosDrive(3,0.5,4000);
+    }
+
+    //Continuation of Autonomous
+
+    //Facing flagpoles
+    private void BlueRover(){
+
+        //Step One Rotate to face wall opposite of scanned image
+        telemetry.addLine("Step One");
+        telemetry.update();
+        Rotate(3,-0.5,2000);
+
+        //Step Two Reverse enough to be in line with depo but not interfering with minerals
+        telemetry.addLine("Step Two");
+        telemetry.update();
+        RunToPosDrive(1,-0.5,2000);
+
+        //Step Three Rotate facing crater
+        telemetry.addLine("Step Three");
+        telemetry.update();
+        Rotate(1,0.5,500);
+
+        //Step Four Reverse and deposit team marker
+        telemetry.addLine("Step Four");
+        telemetry.update();
+        Step4();
+
+        //Step Five Move forward to crater
+        telemetry.addLine("Step Five");
+        telemetry.update();
+        Step5();
+    }
+
+    //Facing wall nearest to Office
+    private void RedFootprint(){
+
+        //Step One Rotate to face wall opposite of scanned image
+        telemetry.addLine("Step One");
+        telemetry.update();
+        Rotate(3,-0.5,2000);
+
+        //Step Two Reverse enough to be in line with depo but not interfering with minerals
+        telemetry.addLine("Step Two");
+        telemetry.update();
+        RunToPosDrive(1,-0.5,2000);
+
+        //Step Three Rotate facing crater
+        telemetry.addLine("Step Three");
+        telemetry.update();
+        Rotate(1,0.5,500);
+
+        //Step Four Reverse and deposit team marker
+        telemetry.addLine("Step Four");
+        telemetry.update();
+        Step4();
+
+        //Step Five Move forward to crater
+        telemetry.addLine("Step Five");
+        telemetry.update();
+        Step5();
+
+    }
+
+    //Facing away from Robotics door
+    private void FrontCraters(){
+
+        //Step One Rotate to face wall
+        telemetry.addLine("Step One");
+        telemetry.update();
+        Rotate(3,0.5,2000);
+
+        //Step Two Move forward enough to be in line with depo but not interfering with minerals
+        telemetry.addLine("Step Two");
+        telemetry.update();
+        RunToPosDrive(1,0.5,2000);
+
+        //Step Three Rotate facing crater
+        telemetry.addLine("Step Three");
+        telemetry.update();
+        Rotate(1,0.5,500);
+
+        //Step Four Reverse and deposit team marker
+        telemetry.addLine("Step Four");
+        telemetry.update();
+        Step4();
+
+        //Step Five Move forward to crater
+        telemetry.addLine("Step Five");
+        telemetry.update();
+        Step5();
+    }
+
+    //Facing Robotics door
+    private void BackSpace(){
+
+        //Step One Rotate to face wall
+        telemetry.addLine("Step One");
+        telemetry.update();
+        Rotate(3,0.5,2000);
+
+        //Step Two Move forward enough to be in line with depo but not interfering with minerals
+        telemetry.addLine("Step Two");
+        telemetry.update();
+        RunToPosDrive(1,0.5,2000);
+
+        //Step Three Rotate facing crater
+        telemetry.addLine("Step Three");
+        telemetry.update();
+        Rotate(1,0.5,500);
+
+        //Step Four Reverse and deposit team marker
+        telemetry.addLine("Step Four");
+        telemetry.update();
+        Step4();
+
+        //Step Five Move forward to crater
+        telemetry.addLine("Step Five");
+        telemetry.update();
+        Step5();
+
     }
 
 }
