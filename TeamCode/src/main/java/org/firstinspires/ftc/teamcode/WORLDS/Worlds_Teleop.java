d043aa51b3145acb11e9f4d5608322b1cd91aaf8
JavonPeart <JavonPeart@users.noreply.github.com>
2019-03-21 12:28:59 -0500

    Update Worlds_Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/Worlds_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/Worlds_Teleop.java
@@ -8,36 +8,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Worlds Robot", group = "JC")
 
 public class Worlds_Teleop extends LinearOpMode {
-    private DcMotor FrontLeft     = null;
-    private DcMotor FrontRight    = null;
-    private DcMotor BackRight     = null;
-    private DcMotor BackLeft      = null;
-    private DcMotor Lift          = null;
-    private DcMotor Diposite      = null;
-    private DcMotor EnterCrater   = null;
-    private Servo   Spin1         = null;
-    private Servo   Spin2         = null;
-    private Servo   BoxLift1      = null;
-    private Servo   BoxLift2      = null;
-    private Servo   Intake1       = null;
-    private Servo   Intake2       = null;
+    private DcMotor FrontLeft  = null;
+    private DcMotor FrontRight = null;
+    private DcMotor BackRight  = null;
+    private DcMotor BackLeft   = null;
+    private DcMotor Lift       = null;
+    private Servo   Spin1      = null;
+    private Servo   Spin2      = null;
+    private Servo   BoxLift1   = null;
+    private Servo   BoxLift2   = null;
+    private Servo   Intake1    = null;
+    private Servo   Intake2    = null;
 
 
     private final boolean shouldMecanumDrive = true;
 
     @Override
     public void runOpMode() {
-        FrontLeft    = hardwareMap.get(DcMotor.class, "Fleft");
-        FrontRight   = hardwareMap.get(DcMotor.class, "Fright");
-        BackRight    = hardwareMap.get(DcMotor.class, "BRight");
-        BackLeft     = hardwareMap.get(DcMotor.class, "BLeft");
-        Lift         = hardwareMap.get(DcMotor.class, "Lift");
-        Diposite     = hardwareMap.get(DcMotor.class, "Diposite");
-        EnterCrater  = hardwareMap.get(DcMotor.class, "ECrater");
-        BoxLift1     = hardwareMap.get(Servo.class,   "BoxLift1");
-        BoxLift2     = hardwareMap.get(Servo.class,   "BoxLift2");
-        Spin1        = hardwareMap.get(Servo.class,   "Spin1");
-        Spin2        = hardwareMap.get(Servo.class,   "Spin2");
+        FrontLeft  = hardwareMap.get(DcMotor.class, "Fleft");
+        FrontRight = hardwareMap.get(DcMotor.class, "Fright");
+        BackRight  = hardwareMap.get(DcMotor.class, "BRight");
+        BackLeft   = hardwareMap.get(DcMotor.class, "BLeft");
+        Lift       = hardwareMap.get(DcMotor.class, "Lift");
+        BoxLift1   = hardwareMap.get(Servo.class, "BoxLift1");
+        BoxLift2   = hardwareMap.get(Servo.class, "BoxLift2");
+        Spin1      = hardwareMap.get(Servo.class, "Spin1");
+        Spin2      = hardwareMap.get(Servo.class, "Spin2");
 
 
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -45,15 +41,14 @@ public class Worlds_Teleop extends LinearOpMode {
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Diposite.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        EnterCrater.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         waitForStart();
         while (opModeIsActive()) {
             Movement();
+
             LiftControl();
-            DipositeControl();
+
             Intake();
         }
     }
@@ -77,43 +72,38 @@ public class Worlds_Teleop extends LinearOpMode {
     }
 
     private void LiftControl() {
-        if (gamepad1.right_trigger >0.5)
+        if (gamepad1.right_trigger > 0.2)
             Lift.setPower(1);
-        else if (gamepad1.left_trigger >0.5)
+        else if (gamepad1.left_trigger > 0.2)
             Lift.setPower(-1);
         else
             Lift.setPower(0.0);
     }
 
     private void Intake() {
+        //Spinny Part
         if (gamepad2.a) {
             Spin1.setPosition(-1);
             Spin2.setPosition(1);
-        }else
-
-            if (gamepad2.b) {
+        } else if (gamepad2.b) {
             Spin1.setPosition(1);
             Spin2.setPosition(-1);
+        } else {
+            Spin1.setPosition(0.5);
+            Spin2.setPosition(0.5);
+
         }
-    else
-    {
-        Spin1.setPosition(0.5);
-        Spin2.setPosition(0.5);
 
+        //Lifting Part
+        if (gamepad2.dpad_up) {
+            Intake1.setPosition(0);
+            Intake2.setPosition(1);
+        } else if (gamepad2.dpad_down) {
+            Intake1.setPosition(0);
+            Intake2.setPosition(1);
+        } else {
+            Intake1.setPosition(0.5);
+            Intake2.setPosition(0.5);
+        }
     }
-
- if(gamepad2.dpad_up) {
-     Intake1.setPosition(0);
-     Intake2.setPosition(1);
-
- }else
-
-     if(gamepad2.dpad_down)
-         Intake1.setPosition(0);
-     Intake2.setPosition(1);
-
-
-    }
-
-
 }
