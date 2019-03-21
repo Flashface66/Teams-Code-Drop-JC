30e929e7eb9e479494e329cf0a4678513fadd9b1
Flashface66 <flashface66@hotmail.com>
2019-03-21 18:53:47 -0500

    That new Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/HardwareWorlds.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/HardwareWorlds.java
@@ -2,37 +2,61 @@ package org.firstinspires.ftc.teamcode.WORLDS;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * This is the hardware for the 6899 robot or Myles' bot.
+ * This is the hardware for the 3981 robot or Worlds' bot.
  * Add the necessary hardware required here following the format given
  * Do not edit unless you are sure of what you're doing - Chavaughn
  */
+
 public class  HardwareWorlds {
 
     // TODO add the different components of the robots
-    public DcMotor   FrontLeft      = null;
-    public DcMotor   FrontRight     = null;
-    public DcMotor   BackLeft       = null;
-    public DcMotor   BackRight      = null;
-
+    public DcMotor FrontLeft  = null;
+    public DcMotor FrontRight = null;
+    public DcMotor BackRight  = null;
+    public DcMotor BackLeft   = null;
+    public DcMotor Lift       = null;
+    public DcMotor Deposit    = null;
+    public DcMotor Extend     = null;
+    public Servo   Spin1      = null;
+    public Servo   Spin2      = null;
+    public Servo   BoxLift1   = null;
+    public Servo   BoxLift2   = null;
+    public Servo   Intake1    = null;
+    public Servo   Intake2    = null;
+    
+    public HardwareMap hw     = null;
+    
     HardwareWorlds(){
 
     }
 
-    public void init(HardwareMap thehwmap){
+    public void init(HardwareMap hw){
 
         //TODO Initialize new hardware as given below with the drive-train.
-        FrontLeft = thehwmap.get(DcMotor.class,"FrontLeft");
-        FrontRight = thehwmap.get(DcMotor.class,"FrontRight");
-        BackLeft = thehwmap.get(DcMotor.class,"BackLeft");
-        BackRight = thehwmap.get(DcMotor.class,"BackRight");
+        FrontLeft  = hw.get(DcMotor.class, "Fleft");
+        FrontRight = hw.get(DcMotor.class, "Fright");
+        BackRight  = hw.get(DcMotor.class, "BRight");
+        BackLeft   = hw.get(DcMotor.class, "BLeft");
+        Lift       = hw.get(DcMotor.class, "Lift");
+        Deposit    = hw.get(DcMotor.class, "Deposit");
+        Extend     = hw.get(DcMotor.class, "Extend");
+        BoxLift1   = hw.get(Servo.class,   "BoxLift1");
+        BoxLift2   = hw.get(Servo.class,   "BoxLift2");
+        Spin1      = hw.get(Servo.class,   "Spin1");
+        Spin2      = hw.get(Servo.class,   "Spin2");
+        Intake1    = hw.get(Servo.class,   "Intake1");
+        Intake2    = hw.get(Servo.class,   "Intake2");
+
 
         //TODO Make sure directions are correct.
         FrontRight.setDirection(DcMotor.Direction.FORWARD);//Reverse
         FrontLeft.setDirection(DcMotor.Direction.FORWARD);//Forward
-        BackLeft.setDirection(DcMotor.Direction.FORWARD);//Forward
-        BackRight.setDirection(DcMotor.Direction.REVERSE);//Reverse
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);//Forward
+        BackRight.setDirection(DcMotor.Direction.FORWARD);//Reverse
+
 
         //TODO For autonomous make sure to set to Run using encoders
         FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -44,9 +68,10 @@ public class  HardwareWorlds {
         //TODO Add the rest of the components
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Deposit.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         //Setting the initial power of the Motors to 0.
         FrontLeft.setPower(0);
         FrontRight.setPower(0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/Worlds_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/Worlds_Teleop.java
@@ -2,52 +2,20 @@ package org.firstinspires.ftc.teamcode.WORLDS;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+
 
 @TeleOp(name = "Worlds Robot", group = "JC")
 
 public class Worlds_Teleop extends LinearOpMode {
-    private DcMotor FrontLeft  = null;
-    private DcMotor FrontRight = null;
-    private DcMotor BackRight  = null;
-    private DcMotor BackLeft   = null;
-    private DcMotor Lift       = null;
-    private DcMotor Deposit    = null;
-    private DcMotor Extend     = null;
-    private Servo   Spin1      = null;
-    private Servo   Spin2      = null;
-    private Servo   BoxLift1   = null;
-    private Servo   BoxLift2   = null;
-    private Servo   Intake1    = null;
-    private Servo   Intake2    = null;
+
 
 
     private final boolean shouldMecanumDrive = true;
 
+    private HardwareWorlds hw  = new HardwareWorlds();
+
     @Override
     public void runOpMode() {
-        FrontLeft  = hardwareMap.get(DcMotor.class, "Fleft");
-        FrontRight = hardwareMap.get(DcMotor.class, "Fright");
-        BackRight  = hardwareMap.get(DcMotor.class, "BRight");
-        BackLeft   = hardwareMap.get(DcMotor.class, "BLeft");
-        Lift       = hardwareMap.get(DcMotor.class, "Lift");
-        Deposit    = hardwareMap.get(DcMotor.class, "Deposit");
-        Extend     = hardwareMap.get(DcMotor.class, "Extend");
-        BoxLift1   = hardwareMap.get(Servo.class, "BoxLift1");
-        BoxLift2   = hardwareMap.get(Servo.class, "BoxLift2");
-        Spin1      = hardwareMap.get(Servo.class, "Spin1");
-        Spin2      = hardwareMap.get(Servo.class, "Spin2");
-
-
-
-        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Deposit.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
 
         waitForStart();
         while (opModeIsActive()) {
@@ -71,65 +39,81 @@ public class Worlds_Teleop extends LinearOpMode {
 
             // Convert desired motion to wheel powers, with power clamping.
             Mecanum.Wheels wheels = Mecanum.motionToWheels(motion);
-            FrontLeft.setPower(wheels.frontLeft);
-            FrontRight.setPower(wheels.frontRight);
-            BackLeft.setPower(wheels.backLeft);
-            BackRight.setPower(wheels.backRight);
+            hw.FrontLeft.setPower(wheels.frontLeft);
+            hw.FrontRight.setPower(wheels.frontRight);
+            hw.BackLeft.setPower(wheels.backLeft);
+            hw.BackRight.setPower(wheels.backRight);
         }
 
     }
 
     private void LiftControl() {
-        if (gamepad1.right_trigger > 0.2)
-            Lift.setPower(1);
-        else if (gamepad1.left_trigger > 0.2)
-            Lift.setPower(-1);
-        else
-            Lift.setPower(0.0);
+        if (gamepad1.right_trigger > 0.2){
+            hw.Lift.setPower(1);}
+        else if (gamepad1.left_trigger > 0.2){
+            hw.Lift.setPower(-1);}
+        else{
+            hw.Lift.setPower(0.0);}
     }
 
     private void Intake() {
         //Spinny Part
         if (gamepad2.a) {
-            Spin1.setPosition(-1);
-            Spin2.setPosition(1);
-        } else if (gamepad2.b) {
-            Spin1.setPosition(1);
-            Spin2.setPosition(-1);
-        } else {
-            Spin1.setPosition(0.5);
-            Spin2.setPosition(0.5);
+            hw.Spin1.setPosition(0);
+            hw.Spin2.setPosition(1);
+        }
+        if (gamepad2.b) {
+            hw.Spin1.setPosition(1);
+            hw.Spin2.setPosition(0);
+        }
+        if (gamepad2.x){
+            hw.Spin1.setPosition(0.5);
+            hw.Spin2.setPosition(0.5);
 
         }
 
-        //Lifting Part
+        //Lifts Spinny Part
         if (gamepad2.dpad_up) {
-            Intake1.setPosition(0);
-            Intake2.setPosition(1);
-        } else if (gamepad2.dpad_down) {
-            Intake1.setPosition(0);
-            Intake2.setPosition(1);
-        } else {
-            Intake1.setPosition(0.5);
-            Intake2.setPosition(0.5);
+            hw.Intake1.setPosition(0);
+            hw.Intake2.setPosition(1);
+        }
+        if (gamepad2.dpad_down) {
+            hw.Intake1.setPosition(0);
+            hw.Intake2.setPosition(1);
         }
     }
 
     private void Arms(){
         //Lifts to the Lander
-        if (gamepad2.right_bumper)
-            Deposit.setPower(1);
-        else if (gamepad2.left_bumper)
-            Deposit.setPower(-1);
-        else
-            Deposit.setPower(0.0);
+         if (gamepad2.right_trigger >0.3){
+           hw.Deposit.setPower(gamepad2.right_trigger);}
+        else if (gamepad2.left_trigger >0.3){
+            hw.Deposit.setPower(-gamepad2.left_trigger);}
+        else{
+            hw.Deposit.setPower(0.0);}
 
         //Extend into the Crater
-        if (gamepad2.right_stick_y > 0.2)
-            Extend.setPower(1);
-        else if (gamepad2.right_stick_y < -0.2)
-            Extend.setPower(-1);
-        else
-            Extend.setPower(0);
+        if (gamepad2.right_stick_y > 0.2){
+            hw.Extend.setPower(1);}
+        else if (gamepad2.right_stick_y < -0.2){
+            hw.Extend.setPower(-1);}
+        else{
+            hw.Extend.setPower(0.0);}
+
+            //Box Lift Servos
+        if (gamepad2.right_bumper) {
+            hw.BoxLift1.setPosition(0.2);
+            hw.BoxLift2.setPosition(0.8);
+        }
+        if (gamepad2.left_bumper) {
+            hw.BoxLift1.setPosition(0.8);
+            hw.BoxLift2.setPosition(0.2);
+        }
+
+        }
+
+
     }
-}
+
+
+
