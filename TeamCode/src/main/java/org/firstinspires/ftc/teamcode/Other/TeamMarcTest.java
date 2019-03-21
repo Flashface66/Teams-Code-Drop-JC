54d1e554ce12eab630156db8f183bfae2232e0b6
Flashface66 <flashface66@hotmail.com>
2019-03-21 11:55:43 -0500

    YoYo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeamMarcTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeamMarcTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Other;
 
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/Worlds_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WORLDS/Worlds_Teleop.java
@@ -4,63 +4,62 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.teamcode.Mecanum;
 
 @TeleOp(name = "Worlds Robot", group = "JC")
 
 public class Worlds_Teleop extends LinearOpMode {
-    private DcMotor FrontLeft  = null;
-    private DcMotor FrontRight = null;
-    private DcMotor BackRight  = null;
-    private DcMotor BackLeft   = null;
-    private DcMotor HookLift   = null;
-    private DcMotor Diposite   = null;
-    private DcMotor EnterCrater= null;
-    private Servo   BoxLift1;
-    private Servo   BoxLift2;
+    private DcMotor FrontLeft     = null;
+    private DcMotor FrontRight    = null;
+    private DcMotor BackRight     = null;
+    private DcMotor BackLeft      = null;
+    private DcMotor Lift          = null;
+    private DcMotor Diposite      = null;
+    private DcMotor EnterCrater   = null;
+    private Servo   Spin1         = null;
+    private Servo   Spin2         = null;
+    private Servo   BoxLift1      = null;
+    private Servo   BoxLift2      = null;
+    private Servo   Intake1       = null;
+    private Servo   Intake2       = null;
 
 
     private final boolean shouldMecanumDrive = true;
 
     @Override
-    public void runOpMode(){
-        FrontLeft  = hardwareMap.get(DcMotor.class,"Fleft");
-        FrontRight = hardwareMap.get(DcMotor.class,"Fright");
-        BackRight  = hardwareMap.get(DcMotor.class,"BRight");
-        BackLeft   = hardwareMap.get(DcMotor.class,"BLeft");
-        HookLift   = hardwareMap.get(DcMotor.class, "HLift");
-        Diposite   = hardwareMap.get(DcMotor.class, "Diposite");
-        EnterCrater= hardwareMap.get(DcMotor.class, "ECrater");
-
-        BoxLift1   = hardwareMap.get(Servo.class, "BoxLift1");
-        BoxLift2   = hardwareMap.get(Servo.class, "BoxLift2");
-
-
+    public void runOpMode() {
+        FrontLeft    = hardwareMap.get(DcMotor.class, "Fleft");
+        FrontRight   = hardwareMap.get(DcMotor.class, "Fright");
+        BackRight    = hardwareMap.get(DcMotor.class, "BRight");
+        BackLeft     = hardwareMap.get(DcMotor.class, "BLeft");
+        Lift         = hardwareMap.get(DcMotor.class, "Lift");
+        Diposite     = hardwareMap.get(DcMotor.class, "Diposite");
+        EnterCrater  = hardwareMap.get(DcMotor.class, "ECrater");
+        BoxLift1     = hardwareMap.get(Servo.class,   "BoxLift1");
+        BoxLift2     = hardwareMap.get(Servo.class,   "BoxLift2");
+        Spin1        = hardwareMap.get(Servo.class,   "Spin1");
+        Spin2        = hardwareMap.get(Servo.class,   "Spin2");
 
 
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        HookLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         Diposite.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         EnterCrater.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        BoxLift1.
 
         waitForStart();
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
             Movement();
             LiftControl();
             DipositeControl();
-            EnterCraterControl();
+            Intake();
         }
     }
 
 
-    private void Movement(){
+    private void Movement() {
         if (shouldMecanumDrive) {
             // Convert joysticks to desired motion.
             Mecanum.Motion motion = Mecanum.joystickToMotion(
@@ -77,25 +76,44 @@ public class Worlds_Teleop extends LinearOpMode {
 
     }
 
-    private void LiftControl(){
-        if(gamepad2.dpad_up)
-            HookLift.setPower(1);
-        else if (gamepad2.dpad_down)
-            HookLift.setPower(-1);
+    private void LiftControl() {
+        if (gamepad1.right_trigger >0.5)
+            Lift.setPower(1);
+        else if (gamepad1.left_trigger >0.5)
+            Lift.setPower(-1);
         else
-            HookLift.setPower(0);
+            Lift.setPower(0.0);
     }
 
-    private void DipositeControl(){
-        if(gamepad2.b)
-            Diposite.setPower(1);
-        else if (gamepad2.a)
-            Diposite.setPower(-1);
-        else
-            Diposite.setPower(0);
+    private void Intake() {
+        if (gamepad2.a) {
+            Spin1.setPosition(-1);
+            Spin2.setPosition(1);
+        }else
+
+            if (gamepad2.b) {
+            Spin1.setPosition(1);
+            Spin2.setPosition(-1);
+        }
+    else
+    {
+        Spin1.setPosition(0.5);
+        Spin2.setPosition(0.5);
+
     }
 
-    private void EnterCraterControl(){
+ if(gamepad2.dpad_up) {
+     Intake1.setPosition(0);
+     Intake2.setPosition(1);
+
+ }else
+
+     if(gamepad2.dpad_down)
+         Intake1.setPosition(0);
+     Intake2.setPosition(1);
+
 
     }
+
+
 }
